dabbdf31d558c0bb48c2f2eff1a40f50bd87a6f1
fleur101 <gaukhard96@gmail.com>
2019-03-16 01:01:34 +0600

    created navigation drawer

--- app/src/main/java/com/example/fleur101/epambooksapp/MainActivity.java
+++ app/src/main/java/com/example/fleur101/epambooksapp/MainActivity.java
@@ -1,13 +1,119 @@
 package com.example.fleur101.epambooksapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    public static final String TAG = "MAIN_ACTIVITY_TAG";
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true);
+        }
+        if (actionbar != null) {
+            actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        Fragment fragment = new SearchFragment();
+
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, fragment).commit();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        drawerLayout.closeDrawers();
+                        int id = menuItem.getItemId();
+                        Fragment fragment = null;
+                        switch(id){
+                            case R.id.nav_search:
+                                fragment = new SearchFragment();
+                                break;
+                            case R.id.nav_books:
+                                fragment = new MyBooksFragment();
+                                break;
+                            case R.id.nav_requests:
+                                fragment = new MyRequestsFragment();
+                                break;
+                            case R.id.nav_settings:
+                                fragment = new SettingsFragment();
+                                break;
+                            case R.id.nav_logout:
+                                break;
+                        }
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                        return true;
+                    }});
+
+         drawerLayout.addDrawerListener(
+                 new DrawerLayout.DrawerListener() {
+                        @Override
+                        public void onDrawerSlide(View drawerView, float slideOffset) {
+                            // Respond when the drawer's position changes
+                        }
+
+                        @Override
+                        public void onDrawerOpened(View drawerView) {
+                            // Respond when the drawer is opened
+                        }
+
+                        @Override
+                        public void onDrawerClosed(View drawerView) {
+                            // Respond when the drawer is closed
+                        }
+
+                        @Override
+                        public void onDrawerStateChanged(int newState) {
+                            // Respond when the drawer motion state changes
+                        }
+                    }
+        );
     }
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
