21860d242f5df8c3ab662b2ea47a7ca36e3d3869
Aslan Assylkhanov <AssylkhanovAslan@gmail.com>
2019-03-15 23:01:23 +0600

    Login page base added

---
+++ app/src/main/java/com/example/fleur101/epambooksapp/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.example.fleur101.epambooksapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Assylkhanov Aslan on 15.03.2019.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+
+
+}
