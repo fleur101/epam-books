799cf3cd7fe04fa429dab81005fae8e41e0b9040
Aslan Assylkhanov <AssylkhanovAslan@gmail.com>
2019-03-16 10:37:52 +0600

    Connected Login with Main

--- app/src/main/java/com/example/fleur101/epambooksapp/LoginActivity.java
+++ app/src/main/java/com/example/fleur101/epambooksapp/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.fleur101.epambooksapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 /**
  * Created by Assylkhanov Aslan on 15.03.2019.
@@ -13,5 +15,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button btnGoogle = findViewById(R.id.btn_google);
+        Button btnFacebook = findViewById(R.id.btn_facebook);
+        btnGoogle.setOnClickListener(view -> startActivity(new Intent(this, MainActivity.class)));
+        btnFacebook.setOnClickListener(view -> startActivity(new Intent(this, MainActivity.class)));
     }
 }
