5a426103684834f6aa7268822a9765ba715c5d6b
fleur101 <gaukhard96@gmail.com>
2019-03-16 18:55:43 +0600

    retrieve firebase books

--- app/src/main/java/com/example/fleur101/epambooksapp/BooksAdapter.java
+++ app/src/main/java/com/example/fleur101/epambooksapp/BooksAdapter.java
@@ -1,13 +1,13 @@
 package com.example.fleur101.epambooksapp;
 
-import android.content.Context;
-import android.database.Cursor;
-import android.media.Image;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,52 +18,73 @@ import butterknife.ButterKnife;
  * Created by acer on 16.03.2019.
  */
 
-public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BooksViewHolder> {
+public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BooksAdapterViewHolder> {
 
     private static final String TAG = "BOOKS_ADAPTER_TAG";
-    private Context mContext;
-    private Cursor mCursor;
+    private List<Book> mDataset;
 
 
-
-    public BooksAdapter(Context context, Cursor cursor, MyBooksFragment booksFragment) {
-        this.mContext = context;
-        mCursor = cursor;
-        booksFragment = booksFragment;
-
+    BooksAdapter(List<Book> dataset) {
+        mDataset = dataset;
     }
 
-    public static class BooksViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
+    @NonNull
+    @Override
+    public BooksAdapter.BooksAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.fragment_search_book_item, parent, false);
+        return new BooksAdapterViewHolder(view);
+    }
 
-//        @BindView() ImageView bookImageView;
-        TextView bookNameTextView;
+    @Override
+    public void onBindViewHolder(@NonNull BooksAdapter.BooksAdapterViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: 1");
+        Book book = mDataset.get(position);
+        List<String> authors = book.getAuthors();
+        String authorNames = "";
+        holder.titleTextView.setText(book.getTitle());
+        for (int i=0; i<authors.size(); i++){
+            authorNames+=authors.get(i);
+            authorNames+=", ";
+        }
+        holder.authorTextView.setText(authorNames);
+        holder.publisher.setText(book.getPublisher());
+        holder.publishDate.setText(book.getPublish_date().toString());
+        Log.d(TAG, "onBindViewHolder: 2");
+    }
+
+    public void setData(List<Book> dataset){
+        Log.d(TAG, "setData: ");
+        mDataset = dataset;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    class BooksAdapterViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.image_book)
+        ImageView bookImageView;
+        @BindView(R.id.tv_title)
+        TextView titleTextView;
+        @BindView(R.id.tv_author)
         TextView authorTextView;
+        @BindView(R.id.tv_publisher)
         TextView publisher;
+        @BindView(R.id.tv_publish_date)
         TextView publishDate;
+        @BindView(R.id.iv_request_icon)
+        ImageView requestIcon;
+        @BindView(R.id.iv_info_icon)
+        ImageView infoIcon;
 
-        public BooksViewHolder(TextView v) {
+        private BooksAdapterViewHolder(View v) {
             super(v);
             ButterKnife.bind(this, itemView);
         }
     }
 
 
-
-    @NonNull
-    @Override
-    public BooksAdapter.BooksViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull BooksAdapter.BooksViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
 }
--- app/src/main/java/com/example/fleur101/epambooksapp/SearchFragment.java
+++ app/src/main/java/com/example/fleur101/epambooksapp/SearchFragment.java
@@ -1,27 +1,88 @@
 package com.example.fleur101.epambooksapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class SearchFragment extends BaseFragment {
+
+    public static final String TAG = "SEARCH_FRAGMENT_TAG";
+    private FirebaseFirestore db;
+    private List<Book> mDataset;
+    private BooksAdapter mAdapter;
+    @BindView(R.id.rv_my_books) RecyclerView recyclerView;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+//        if (getArguments() != null) {
 //            mParam1 = getArguments().getString(ARG_PARAM1);
 //            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        ButterKnife.bind(this, view);
+        // using Linear Layout Manager
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        mDataset = new ArrayList<>();
+
+        // using Linear Layout Manager
+        mAdapter = new BooksAdapter(mDataset);
+        recyclerView.setAdapter(mAdapter);
+
+//        getData();
+        return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+        getData();
+    }
+
+    public void getData(){
+        db = FirebaseFirestore.getInstance();
+        db.collection("books")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        List<Book> data = new ArrayList<>();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Book book = document.toObject(Book.class);
+//                            mDataset.add(book);
+                            data.add(book);
+                            Log.d(TAG, document.getId() + " => " + document.getData());
+                        }
+
+                        mAdapter.setData(data);
+                    } else {
+                        Log.w(TAG, "Error getting documents.", task.getException());
+                    }
+                });
+    }
+
 
 }
