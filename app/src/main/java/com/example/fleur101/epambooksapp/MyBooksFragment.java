ddf7a079c8c2316f8d91bc43563b46bf3c1adb74
kenzhemir <miras.kenzhegaliyev@nu.edu.kz>
2019-03-17 04:23:25 +0600

    Added api for isbn search

--- app/src/main/java/com/example/fleur101/epambooksapp/MyBooksFragment.java
+++ app/src/main/java/com/example/fleur101/epambooksapp/MyBooksFragment.java
@@ -1,26 +1,57 @@
 package com.example.fleur101.epambooksapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 public class MyBooksFragment extends BaseFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            //mParam1 = getArguments().getString(ARG_PARAM1);
-            //mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_books, container, false);
+        View view =inflater.inflate(R.layout.fragment_my_books, container, false);
+        FloatingActionButton floatingActionButton = view.findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(view1 -> startActivityForResult(new Intent(getContext(), ScannerAcitivity.class), 1));
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+
+            if (resultCode == RESULT_OK) {
+                String barcode = data.getStringExtra("barcode");
+                setText(barcode);
+                //your code
+
+            }
+            if (resultCode == RESULT_CANCELED) {
+                // Write your code if there's no result
+                Toast.makeText(getContext(), "Try again", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void setText(String text){
+        TextView t = (TextView)getView().findViewById(R.id.result_text);
+        t.setText(text);
     }
 
 }
